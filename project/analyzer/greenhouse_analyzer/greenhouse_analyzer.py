575e403ff4db208a11896de722ad4d900dfe8d83
CarlinoCalogero <calogerocarlino19@gmail.com>
2025-01-14 23:39:40 +0100

    added analyzer

---
+++ project/analyzer/greenhouse_analyzer/greenhouse_analyzer.py
@@ -0,0 +1,9 @@
+from analyzer import Analyzer
+
+class GreenhouseAnalyzer(Analyzer):
+
+    def __init__(self):
+        super().__init__()
+
+    def __predictNextValues(self, values, window_size, num_predictions):
+        pass
